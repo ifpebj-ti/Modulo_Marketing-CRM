cfb8e4861631b8a1e5e2aef55d491cb4477757a1
ALYSSON P. ASSUNÇÃO <20192ewbj0191@belojardim.ifpe>
2023-09-06 20:11:47 -0300

    test model cliente, item e marketing

---
+++ ModuloMarketing.Test/Models/ItemTest.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+public class ItemTest
+{
+
+    public Item item {get; set; } = new Item{
+        Venda = new Venda(),
+        Produto = new Produto(),
+        Quantidade = 5,
+        PrecoUnitario = 12.05
+    };
+
+    [Fact]
+    public void TestHasAll()
+    {
+        Assert.Equal(5, item.Quantidade);
+        var validationContext = new ValidationContext(item);
+        var validationResults = new List<ValidationResult>();
+        bool isValid = Validator.TryValidateObject(item, validationContext, validationResults, true);
+        Assert.True(isValid);
+    }
+    
+}
