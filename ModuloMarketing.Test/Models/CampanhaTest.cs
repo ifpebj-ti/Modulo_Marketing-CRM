84bdb4727a0acb09832ba081b07adcd7505659df
ALYSSON P. ASSUNÇÃO <20192ewbj0191@belojardim.ifpe>
2023-09-26 23:14:35 -0300

    correção dos testes

---
+++ ModuloMarketing.Test/Models/CampanhaTest.cs
@@ -0,0 +1,57 @@
+using System.ComponentModel.DataAnnotations;
+
+public class CampanhaTest
+{
+
+    public Campanha campanha {get; set; } = new Campanha{
+        Nome_Campanha = "Campanha name",
+        Descricao = "Test description",
+        Email_Criador = "Test@test.com",
+        Data_Inicio = DateTime.Now,
+        Data_Termino = DateTime.Now
+    };
+
+    [Fact]
+    public void TestHasAll()
+    {
+        Assert.Equal("Campanha name", campanha.Nome_Campanha);
+        var validationContext = new ValidationContext(campanha);
+        var validationResults = new List<ValidationResult>();
+        bool isValid = Validator.TryValidateObject(campanha, validationContext, validationResults, true);
+        Assert.True(isValid);
+    }
+
+    [Fact]
+    public void CampanhaSemNome()
+    {
+        campanha.Nome_Campanha = "";
+        var validationContext = new ValidationContext(campanha);
+        var validationResults = new List<ValidationResult>();
+        bool isValid = Validator.TryValidateObject(campanha, validationContext, validationResults, true);
+        Assert.False(isValid);
+        Assert.Equal("The Nome_Campanha field is required.", validationResults[0].ErrorMessage);
+    }
+
+    [Fact]
+    public void CampanhaSemDescricao()
+    {
+        campanha.Descricao = "";
+        var validationContext = new ValidationContext(campanha);
+        var validationResults = new List<ValidationResult>();
+        bool isValid = Validator.TryValidateObject(campanha, validationContext, validationResults, true);
+        Assert.False(isValid);
+        Assert.Equal("The Descricao field is required.", validationResults[0].ErrorMessage);
+    }
+
+    [Fact]
+    public void CampanhaSemEmailCriador()
+    {
+        campanha.Email_Criador = "";
+        var validationContext = new ValidationContext(campanha);
+        var validationResults = new List<ValidationResult>();
+        bool isValid = Validator.TryValidateObject(campanha, validationContext, validationResults, true);
+        Assert.False(isValid);
+        Assert.Equal("The Email_Criador field is required.", validationResults[0].ErrorMessage);
+    }
+
+}
