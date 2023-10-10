376979c8c44b2002d22bf7d22378c33f0c1782a3
Everton <evertonlucaslg@gmail.com>
2023-10-09 21:44:29 -0300

    Feat: Adicionando nova tabela para armazenar datas comemorativas de celebração

---
+++ ModuloMarketing.Api/Domain/DataComemorativa.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModuloMarketing.Api.Domain
+{
+    public class DataComemorativa
+    {
+        [Key]
+        public int Id_Comemorativa { get; set; }
+        [Required]
+        public string Nome_Data { get; set; }
+        [Required]
+        public int Dia { get; set; }
+        [Required]
+        public int Mes { get; set; }
+
+    }
+}
---
+++ ModuloMarketing.Api/Migrations/20231010004315_AdicionandoDataComemorativa.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ModuloMarketing.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionandoDataComemorativa : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DataComemorativa",
+                columns: table => new
+                {
+                    Id_Comemorativa = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome_Data = table.Column<string>(type: "text", nullable: false),
+                    Dia = table.Column<int>(type: "integer", nullable: false),
+                    Mes = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DataComemorativa", x => x.Id_Comemorativa);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DataComemorativa");
+        }
+    }
+}
